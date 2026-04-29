cad534e1011f8af665be824a0d04fce8dfbfd144
Nata Tarasova <na.tara.s@yandex.ru>
2026-04-29 21:43:48 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,18 @@
+# Базовый URL стенда (убедись, что он актуален)
+URL_SERVICE = "https://88eadaea-191e-4ac6-9a02-86f072c21eff.serverhub.praktikum-services.ru"
+
+# Путь к документации
+DOC_PATH = "/docs/"
+
+# Путь к логам
+LOG_MAIN_PATH = "/api/logs/main/"
+
+# Путь к таблице в БД
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+# Путь на создание пользователя
+CREATE_USER_PATH = "/api/v1/users/"
+
+# Путь на поиск наборов по продуктам
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
